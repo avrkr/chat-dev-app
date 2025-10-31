25ec59f6de2bd7f1eb1d8f5cf426b91f440fa388
Venkata Rahul Kumar <venkatarahul@pranathiss.local>
2025-10-31 14:29:22 +0530

    Ready For Deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 
 dotenv.config();
 
-export const app = express();
+const app = express();
+const __dirname = path.resolve();
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
+//make ready for deployment
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (_, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
